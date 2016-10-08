6150d026625ca027ce275597dbf99f497945167d
Dylan A. Parker <dylanaparkerdev@gmail.com>
2016-10-08 16:15:29 -0700

    removed html from app.py added templates

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -6,30 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<name>')
 def index(name="world"):
-    return """
-    <!doctype html>
-    <head>
-        <title>Hello, {}</title>
-    </head>
-    <body>
-        <h1>Hello, {}</h1>
-    </body>
-    """.format(name, name)
-
+    return render_template("hello.html", name=name)
+    # could have also been written as:
+    # context = {'name': name}
+    # return render_template("hello.html", **context)
 @app.route('/add/<int:num1>/<int:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-    return """
-    <!doctype html>
-    <head>
-        <title>We're Adding!</title>
-    </head>
-    <body>
-        <h1>{} + {} = {}</h1>
-    </body>
-    """.format(num1, num2, num1+num2)
+    context = {'num1': num1, 'num2': num2}
+    return render_template("add.html", **context)
 
 
 app.run(debug=True, port=8000)
