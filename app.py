3ee35c83c7c6dc47bd278c8214e530a2f6334319
Dylan A. Parker <dylanaparkerdev@gmail.com>
2016-10-08 15:36:06 -0700

    added functionality to the add route for adding floats as well as integers

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ def index(name="world"):
     return "Hello, {}!".format(name)
 
 @app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
     return str(num1+num2)
 
