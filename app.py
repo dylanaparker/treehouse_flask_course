7193ced09ac8be5dd4f68d40e460d62004376d35
Dylan A. Parker <dylanaparkerdev@gmail.com>
2016-10-08 15:50:05 -0700

    All routes now return html

--- app.py
+++ app.py
@@ -6,14 +6,30 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<name>')
 def index(name="world"):
-    return "Hello, {}!".format(name)
+    return """
+    <!doctype html>
+    <head>
+        <title>Hello, {}</title>
+    </head>
+    <body>
+        <h1>Hello, {}</h1>
+    </body>
+    """.format(name, name)
 
 @app.route('/add/<int:num1>/<int:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-    return str(num1+num2)
+    return """
+    <!doctype html>
+    <head>
+        <title>We're Adding!</title>
+    </head>
+    <body>
+        <h1>{} + {} = {}</h1>
+    </body>
+    """.format(num1, num2, num1+num2)
 
 
 app.run(debug=True, port=8000)
